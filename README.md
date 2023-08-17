4a386a527fc2d6d8c1b80bbb7810232000c72d7b
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-17 18:48:28 -0500

    readme update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SQL: Employee Tracker CLI Application • ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+# Employee Tracker CLI Application • SQL
 
 ## Table of Contents
 
--- models/Department.js
+++ models/Department.js
@@ -25,30 +25,3 @@ class Department {
 }
 
 module.exports = Department;
-const db = require("../db/connection");
-
-class Department {
-  constructor(name) {
-    this.name = name;
-  }
-
-  async getAll() {
-    const sql = "SELECT name, department_id FROM departments";
-    const [rows] = await db.promise().query(sql);
-    return rows;
-  }
-
-  addDepartment() {
-    const sql = `INSERT INTO departments (name)
-    VALUES ("${this.name}")`;
-    db.query(sql, (err, rows) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("Department added!");
-    });
-  }
-}
-
-module.exports = Department;
