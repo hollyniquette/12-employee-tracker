9bd02885976e8b88e2cb0c12f1feed987da6ceef
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-17 18:51:08 -0500

    fixed

--- models/Role.js
+++ models/Role.js
@@ -1,26 +1,6 @@
 const db = require("../db/connection");
 
 class Role {
-  constructor(role_id, title, salary, department_id) {
-    this.role_id, this.title, this.salary, this.department_id;
-  }
-
-  getAll() {
-    const sql = "SELECT * FROM roles";
-    return db.query(sql);
-  }
-
-  addRole() {
-    const sql = "INSERT INTO roles (title, salary) VALUES (?,?)";
-    const params = [this.title, this.salary];
-    return db.query(sql, params);
-  }
-}
-
-module.exports = Role;
-const db = require("../db/connection");
-
-class Role {
   constructor(title, salary, department_id) {
     (this.title = title),
       (this.salary = salary),
